0a638cc02b3cced9734552e282a436f7a53d1f6c
chrisdaid <c.chrisdb@gmail.com>
2021-08-08 05:04:39 -0400

    Co-authored-by: cosmic <cosmxc@users.noreply.github.com>

--- static/app.js
+++ static/app.js
@@ -7,7 +7,7 @@ document.getElementById('polish-button').addEventListener('mouseup', async () =>
     console.log("Json", json);
 
     const suggestions = json.corrected_words;
-    
+
     var offset = 0;
     for (const suggestion of Object.values(suggestions)) {
         var diff = suggestion.corrected.length - suggestion.initial.length;
@@ -18,4 +18,94 @@ document.getElementById('polish-button').addEventListener('mouseup', async () =>
         // [ { index: [start, end] } ]
     }
     textarea.value = text;
+})
+
+window.addEventListener("load", () => {
+    console.log("WORKING");
+    const textarea_old = document.getElementsByTagName("textarea").item(0);
+    const rect = textarea_old.getBoundingClientRect();
+    const textarea = textarea_old.cloneNode();
+    const parentTextarea = textarea_old.parentElement;
+    // parentTextarea.removeChild(textarea);
+    const wrapper = document.createElement('div');
+    const overlay = document.createElement('div');
+    const highlights = document.createElement('div');
+    highlights.className = "writeit-highlights";
+    overlay.className = "writeit-highlight-overlay";
+    overlay.appendChild(highlights);
+
+    wrapper.className = "writeit-textarea-wrapper";
+    wrapper.appendChild(overlay);
+    console.log("Contains? " + parentTextarea.contains(textarea_old));
+    console.log(wrapper);
+    console.log(textarea_old.toString() + 'wrapper: ' + wrapper.toString());
+    textarea_old.replaceWith(wrapper);
+    wrapper.appendChild(textarea);
+    
+    var $container = $('.writeit-textarea-wrapper');
+    var $backdrop = $('.writeit-highlight-overlay');
+    var $highlights = $('.writeit-highlights');
+    var $textarea = $('textarea');
+    // var $toggle = $('button');
+
+    // yeah, browser sniffing sucks, but there are browser-specific quirks to handle that are not a matter of feature detection
+    var ua = window.navigator.userAgent.toLowerCase();
+    var isIE = !!ua.match(/msie|trident\/7|edge/);
+    var isWinPhone = ua.indexOf('windows phone') !== -1;
+    var isIOS = !isWinPhone && !!ua.match(/ipad|iphone|ipod/);
+
+    function applyHighlights(text) {
+        text = text
+            .replace(/\n$/g, '\n\n')
+            .replace(/[A-Z].*?\b/g, '<mark>$&</mark>');
+
+        if (isIE) {
+            text = text.replace(/ /g, ' <wbr>');
+        }
+
+        return text;
+    }
+
+    function handleInput() {
+        var text = $textarea.val();
+        var highlightedText = applyHighlights(text);
+        $highlights.html(highlightedText);
+    }
+
+    function handleScroll() {
+        var scrollTop = $textarea.scrollTop();
+        $backdrop.scrollTop(scrollTop);
+
+        var scrollLeft = $textarea.scrollLeft();
+        $backdrop.scrollLeft(scrollLeft);
+    }
+
+    function fixIOS() {
+        // iOS adds 3px of (unremovable) padding to the left and right of a textarea, so adjust highlights div to match
+        $highlights.css({
+            'padding-left': '+=3px',
+            'padding-right': '+=3px'
+        });
+    }
+
+    function bindEvents() {
+        $textarea.on({
+            'input': handleInput,
+            'scroll': handleScroll
+        });
+
+        $toggle.on('click', function () {
+            $container.toggleClass('perspective');
+        });
+    }
+
+    if (isIOS) {
+        fixIOS();
+    }
+
+    bindEvents();
+    handleInput();
+
+
+
 })
