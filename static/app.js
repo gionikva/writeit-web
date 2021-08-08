e2eaf6b1805ddff2f76e17599f505a15f788aef0
Gionikva <gionikva.2014@gmail.com>
2021-08-08 09:51:57 +0300

    tried to implement correction functionale

---
+++ static/app.js
@@ -0,0 +1,21 @@
+document.getElementById('polish-button').addEventListener('mouseup', async () => {
+    const textarea = document.getElementsByTagName('textarea').item(0);
+    const text = textarea.value;
+    const resp = await fetch({url: '/check/' + text});
+    console.log("Resp", resp);
+    const json = await resp.json();
+    console.log("Json", json);
+
+    const suggestions = json.corrected_words;
+    
+    var offset = 0;
+    for (const suggestion of Object.values(suggestions)) {
+        var diff = suggestion.corrected.length - suggestion.initial.length;
+        var bef = text.slice(0, suggestion.index[0] + offset);
+        var aft = text.slice(suggestion.index[1] + offset);
+        text = bef + suggestion.corrected + aft;
+        offset += diff;
+        // [ { index: [start, end] } ]
+    }
+    textarea.value = text;
+})
